678f894c4af6d1274ef40ad68c05b56ee2fe0dba
avacy04-code <a.vacy04@gmail.com>
2026-03-10 17:46:53 +0100

    Refactor badge checking logic and question display

--- script.js
+++ script.js
@@ -1,123 +1,21 @@
-let preguntas=[
+function comprobarInsignias(){
 
-{
+if(puntos>=30){
 
-pregunta:"¿Cuántos minutos de actividad física se recomiendan al día?",
-
-respuestas:["20","30","60","120"],
-
-correcta:2
-
-},
-
-{
-
-pregunta:"¿Qué debemos hacer antes de hacer ejercicio?",
-
-respuestas:["Dormir","Calentar","Comer dulces","Nada"],
-
-correcta:1
-
-},
-
-{
-
-pregunta:"¿Qué mejora la resistencia?",
-
-respuestas:["Correr","Dormir","Videojuegos","Comer pizza"],
-
-correcta:0
+document.getElementById("insignias").innerHTML+="🥗 Experto saludable<br>"
 
 }
 
-]
+if(puntos>=60){
 
-let actual=0
-
-let puntos=0
-
-function mostrarPregunta(){
-
-let p=preguntas[actual]
-
-document.getElementById("pregunta").innerText=p.pregunta
-
-for(let i=0;i<4;i++){
-
-document.getElementById("r"+i).innerText=p.respuestas[i]
+document.getElementById("insignias").innerHTML+="🏃 Maestro del movimiento<br>"
 
 }
 
-iniciarTiempo()
+if(puntos>=100){
 
-}
-
-function responder(opcion){
-
-let p=preguntas[actual]
-
-if(opcion===p.correcta){
-
-puntos+=10
-
-sonidoAcierto()
-
-comprobarInsignias()
-
-}else{
-
-sonidoError()
-
-}
-
-document.getElementById("puntos").innerText=puntos
-
-actual++
-
-setTimeout(()=>{
-
-if(actual<preguntas.length){
-
-mostrarPregunta()
-
-}else{
-
-document.getElementById("pregunta").innerText="Juego terminado"
-
-}
-
-},3000)
-
-}
-
-function iniciarTiempo(){
-
-let tiempo=10
-
-let barra=document.getElementById("timer")
-
-let contador=setInterval(()=>{
-
-tiempo--
-
-barra.style.width=(tiempo*10)+"%"
-
-if(tiempo<=0){
-
-clearInterval(contador)
-
-actual++
-
-if(actual<preguntas.length){
-
-mostrarPregunta()
+document.getElementById("insignias").innerHTML+="🏆 Super atleta<br>"
 
 }
 
 }
-
-},1000)
-
-}
-
-mostrarPregunta()
