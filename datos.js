55f7851f1623749f1dd21376a144823c82d634cc
avacy04-code <a.vacy04@gmail.com>
2026-03-11 23:49:53 +0100

    Update datos.js

--- datos.js
+++ datos.js
@@ -16,8 +16,97 @@ correcta:1
 pregunta:"¿Qué ejercicio mejora la resistencia?",
 respuestas:["Correr","Dormir","Videojuegos","TV"],
 correcta:0
-}
+},
 
+  {
+pregunta:"¿Cuántas horas de sueño se recomiendan para adolescentes?",
+respuestas:["4-5 horas","6 horas","8-10 horas","12 horas"],
+correcta:2
+},
+
+{
+pregunta:"¿Cuál es la bebida más saludable durante el ejercicio?",
+respuestas:["Refresco","Agua","Bebida energética","Zumo azucarado"],
+correcta:1
+},
+
+{
+pregunta:"¿Cuántas comidas principales se recomiendan al día?",
+respuestas:["1","2","3-5","7"],
+correcta:2
+},
+
+{
+pregunta:"¿Qué grupo de alimentos aporta más vitaminas?",
+respuestas:["Frutas y verduras","Bollería","Chucherías","Refrescos"],
+correcta:0
+},
+
+{
+pregunta:"¿Qué debemos hacer antes de realizar actividad física?",
+respuestas:["Dormir","Calentar","Comer dulces","Nada"],
+correcta:1
+},
+
+{
+pregunta:"¿Cuál es un buen hábito después del ejercicio?",
+respuestas:["Hidratarse","Ver televisión","Comer chucherías","Dormir en el suelo"],
+correcta:0
+},
+
+{
+pregunta:"¿Cuántos minutos de actividad física recomienda la OMS al día?",
+respuestas:["20","30","60","120"],
+correcta:2
+},
+
+{
+pregunta:"¿Qué hábito ayuda a prevenir lesiones?",
+respuestas:["Calentar","Dormir poco","No estirar","Comer rápido"],
+correcta:0
+},
+
+{
+pregunta:"¿Qué tipo de alimento da energía para hacer deporte?",
+respuestas:["Carbohidratos","Refrescos","Chucherías","Grasas trans"],
+correcta:0
+},
+
+{
+pregunta:"¿Qué debemos hacer después de hacer ejercicio?",
+respuestas:["Estirar","Dormir en el suelo","Comer dulces","Nada"],
+correcta:0
+},
+
+{
+pregunta:"¿Cuánta agua aproximadamente debemos beber al día?",
+respuestas:["1 vaso","3 litros de refresco","1,5-2 litros","Nada"],
+correcta:2
+},
+
+{
+pregunta:"¿Qué hábito mejora la salud mental?",
+respuestas:["Actividad física","Dormir poco","Pantallas todo el día","Comer dulces"],
+correcta:0
+},
+
+{
+pregunta:"¿Qué alimento es mejor para un desayuno saludable?",
+respuestas:["Fruta","Bollería","Chucherías","Refresco"],
+correcta:0
+},
+
+{
+pregunta:"¿Qué debemos evitar antes de dormir?",
+respuestas:["Pantallas","Leer","Relajarse","Respirar profundo"],
+correcta:0
+},
+
+{
+pregunta:"¿Qué hábito ayuda a cuidar la espalda?",
+respuestas:["Buena postura","Encogerse","Sentarse mal","Dormir sentado"],
+correcta:0
+}
 ]
 
 let retos=[
