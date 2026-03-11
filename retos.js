fd54ec6ef4b9b2c014852b884fc95edf1bca693c
avacy04-code <a.vacy04@gmail.com>
2026-03-11 23:09:59 +0100

    Create retos.js

---
+++ retos.js
@@ -0,0 +1,14 @@
+let retos = [
+
+"10 burpees",
+"20 jumping jacks",
+"30 segundos de plancha",
+"15 sentadillas",
+"equilibrio sobre un pie 20s",
+"carrera hasta la pared y volver",
+"10 flexiones",
+"20 saltos de comba",
+"reto cooperativo con balón",
+"10 saltos laterales",
+
+]
