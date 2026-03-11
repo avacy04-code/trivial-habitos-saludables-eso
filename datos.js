af533bd9cbdde4c20c65de3f4330798f48f11e1e
avacy04-code <a.vacy04@gmail.com>
2026-03-11 23:36:17 +0100

    Create datos.js

---
+++ datos.js
@@ -0,0 +1,43 @@
+let preguntas=[
+
+{
+pregunta:"¿Cuántos minutos de actividad física recomienda la OMS?",
+respuestas:["30","45","60","90"],
+correcta:2
+},
+
+{
+pregunta:"¿Qué debemos hacer antes de hacer ejercicio?",
+respuestas:["Dormir","Calentar","Comer dulces","Nada"],
+correcta:1
+},
+
+{
+pregunta:"¿Qué ejercicio mejora la resistencia?",
+respuestas:["Correr","Dormir","Videojuegos","TV"],
+correcta:0
+}
+
+]
+
+let retos=[
+
+"10 burpees",
+"15 sentadillas",
+"30 segundos plancha",
+"20 jumping jacks",
+"equilibrio 20 segundos",
+"carrera hasta la pared",
+"10 flexiones",
+"20 saltos de comba"
+
+]
+
+let equipos=[
+
+{nombre:"Dragón",puntos:0},
+{nombre:"Tigre",puntos:0},
+{nombre:"Águila",puntos:0},
+{nombre:"Tiburón",puntos:0}
+
+]
