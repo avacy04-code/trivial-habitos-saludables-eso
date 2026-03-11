0e21b512c1cd2ba942c90e282771da4f05da8466
avacy04-code <a.vacy04@gmail.com>
2026-03-11 23:05:28 +0100

    Refactor equipoSeleccionado and ranking functions
    Removed unused variable and cleaned up the ranking display function.

--- equipos.js
+++ equipos.js
@@ -1,37 +1,12 @@
-let equipoSeleccionado=null
-
 let ranking=[
-
 {nombre:"Dragón",puntos:0},
-
 {nombre:"Tigre",puntos:0},
-
 {nombre:"Águila",puntos:0},
-
 {nombre:"Tiburón",puntos:0}
-
 ]
 
 function seleccionarEquipo(i){
 
-equipoSeleccionado=i
-
 alert("Equipo seleccionado: "+ranking[i].nombre)
-
-}
-
-function actualizarRanking(){
-
-let html="<h2>Ranking</h2>"
-
-ranking.sort((a,b)=>b.puntos-a.puntos)
-
-ranking.forEach(e=>{
-
-html+="<p>"+e.nombre+" : "+e.puntos+"</p>"
-
-})
-
-document.getElementById("ranking").innerHTML=html
 
 }
