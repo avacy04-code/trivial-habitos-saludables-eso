0bc0aa113f99b0c0c6b96ea562e953893e91dbce
avacy04-code <a.vacy04@gmail.com>
2026-03-15 17:52:46 +0100

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,132 +1,27 @@
-let preguntas=[
+let preguntas = [
 
 {
-pregunta:"¿Cuántos minutos de actividad física recomienda la OMS?",
-respuestas:["30","45","60","90"],
-correcta:2
-},
-
-{
-pregunta:"¿Qué debemos hacer antes de hacer ejercicio?",
-respuestas:["Dormir","Calentar","Comer dulces","Nada"],
+pregunta:"¿Cuántos minutos de ejercicio se recomiendan al día?",
+respuestas:["10","30","60","120"],
 correcta:1
 },
 
 {
-pregunta:"¿Qué ejercicio mejora la resistencia?",
-respuestas:["Correr","Dormir","Videojuegos","TV"],
-correcta:0
-},
-
-  {
-pregunta:"¿Cuántas horas de sueño se recomiendan para adolescentes?",
-respuestas:["4-5 horas","6 horas","8-10 horas","12 horas"],
-correcta:2
+pregunta:"¿Qué vitamina obtenemos del sol?",
+respuestas:["A","B","C","D"],
+correcta:3
 },
 
 {
-pregunta:"¿Cuál es la bebida más saludable durante el ejercicio?",
-respuestas:["Refresco","Agua","Bebida energética","Zumo azucarado"],
+pregunta:"¿Cuál es una fruta?",
+respuestas:["Pizza","Manzana","Hamburguesa","Patatas"],
 correcta:1
 },
 
 {
-pregunta:"¿Cuántas comidas principales se recomiendan al día?",
-respuestas:["1","2","3-5","7"],
-correcta:2
-},
-
-{
-pregunta:"¿Qué grupo de alimentos aporta más vitaminas?",
-respuestas:["Frutas y verduras","Bollería","Chucherías","Refrescos"],
-correcta:0
-},
-
-{
-pregunta:"¿Qué debemos hacer antes de realizar actividad física?",
-respuestas:["Dormir","Calentar","Comer dulces","Nada"],
+pregunta:"¿Cuántos huesos tiene el cuerpo humano?",
+respuestas:["106","206","306","406"],
 correcta:1
-},
-
-{
-pregunta:"¿Cuál es un buen hábito después del ejercicio?",
-respuestas:["Hidratarse","Ver televisión","Comer chucherías","Dormir en el suelo"],
-correcta:0
-},
-
-{
-pregunta:"¿Cuántos minutos de actividad física recomienda la OMS al día?",
-respuestas:["20","30","60","120"],
-correcta:2
-},
-
-{
-pregunta:"¿Qué hábito ayuda a prevenir lesiones?",
-respuestas:["Calentar","Dormir poco","No estirar","Comer rápido"],
-correcta:0
-},
-
-{
-pregunta:"¿Qué tipo de alimento da energía para hacer deporte?",
-respuestas:["Carbohidratos","Refrescos","Chucherías","Grasas trans"],
-correcta:0
-},
-
-{
-pregunta:"¿Qué debemos hacer después de hacer ejercicio?",
-respuestas:["Estirar","Dormir en el suelo","Comer dulces","Nada"],
-correcta:0
-},
-
-{
-pregunta:"¿Cuánta agua aproximadamente debemos beber al día?",
-respuestas:["1 vaso","3 litros de refresco","1,5-2 litros","Nada"],
-correcta:2
-},
-
-{
-pregunta:"¿Qué hábito mejora la salud mental?",
-respuestas:["Actividad física","Dormir poco","Pantallas todo el día","Comer dulces"],
-correcta:0
-},
-
-{
-pregunta:"¿Qué alimento es mejor para un desayuno saludable?",
-respuestas:["Fruta","Bollería","Chucherías","Refresco"],
-correcta:0
-},
-
-{
-pregunta:"¿Qué debemos evitar antes de dormir?",
-respuestas:["Pantallas","Leer","Relajarse","Respirar profundo"],
-correcta:0
-},
-
-{
-pregunta:"¿Qué hábito ayuda a cuidar la espalda?",
-respuestas:["Buena postura","Encogerse","Sentarse mal","Dormir sentado"],
-correcta:0
 }
-]
-
-let retos=[
-
-"10 burpees",
-"15 sentadillas",
-"30 segundos plancha",
-"20 jumping jacks",
-"equilibrio 20 segundos",
-"carrera hasta la pared",
-"10 flexiones",
-"20 saltos de comba"
-
-]
-
-let equipos=[
-
-{nombre:"Dragón",puntos:0},
-{nombre:"Tigre",puntos:0},
-{nombre:"Águila",puntos:0},
-{nombre:"Tiburón",puntos:0}
 
 ]
