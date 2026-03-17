10ce673bc1b52b7054da430fa7ed8b22169fbf4d
avacy04-code <a.vacy04@gmail.com>
2026-03-17 17:02:49 +0100

    Update ruleta.js

--- ruleta.js
+++ ruleta.js
@@ -1,19 +1,4 @@
-function girarRuleta(){
-
-let ruleta=document.getElementById("ruleta")
-
-let grados=Math.floor(Math.random()*360)+1440
-
-ruleta.style.transform="rotate("+grados+"deg)"
-
-setTimeout(()=>{
-
-let sector=Math.floor((grados%360)/45)
-
-let reto=retos[sector]
-
-document.getElementById("reto").innerText="🎯 RETO: "+reto
-
-},4000)
-
+function obtenerRetoAleatorio() {
+  const indice = Math.floor(Math.random() * retos.length);
+  return retos[indice];
 }
