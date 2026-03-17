72cf9da03f616a63ba201ec6ec50b922bf7f84d8
avacy04-code <a.vacy04@gmail.com>
2026-03-18 00:37:52 +0100

    Update ruleta.js

--- ruleta.js
+++ ruleta.js
@@ -1,4 +1,13 @@
-function obtenerRetoAleatorio() {
-  const indice = Math.floor(Math.random() * retos.length);
-  return retos[indice];
+function obtenerRetoAleatorio(retoUsados = []) {
+  const disponibles = retos.filter((_, i) => !retoUsados.includes(i));
+  const base = disponibles.length ? disponibles : retos;
+  const reto = base[Math.floor(Math.random() * base.length)];
+  return reto;
+}
+
+function obtenerPreguntaAleatoria(preguntasUsadas = []) {
+  const disponibles = preguntas.filter((_, i) => !preguntasUsadas.includes(i));
+  const base = disponibles.length ? disponibles : preguntas;
+  const pregunta = base[Math.floor(Math.random() * base.length)];
+  return pregunta;
 }
